e424fbf2a21bc105df6673cb513d3a966d7c22d7
dorcol1990 <tickets1312@yahoo.com>
2023-11-13 00:15:17 +0100

    finish app

--- src/pages/singleNotePage/SingleNotePage.jsx
+++ src/pages/singleNotePage/SingleNotePage.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import "./SingleNotePage.scss";
+
+import React from 'react';
+import { getAllNotes } from '../../features/notes/noteSlice';
+import { useParams } from 'react-router';
+import { useSelector } from 'react-redux';
 
 const SingleNotePage = () => {
-  return (
-    <div>SingleNotePage</div>
-  )
+    const { id } = useParams();
+    const notes = useSelector(getAllNotes);
+    let tempNote = notes.filter(note => note.noteId === id);
+    return (
+        <section className='note-single-section px-4'>
+            <div className='note-single-title'>
+                <h2 className='my-2 fs-20'>{tempNote[0].noteTitle}</h2>
+            </div>
+            
+            <div className='py-4'>
+                <p>{tempNote[0].noteContent}</p>
+            </div>
+        </section>
+    )
 }
 
 export default SingleNotePage
